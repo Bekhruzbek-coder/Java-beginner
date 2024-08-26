3f14ae8024a6996f345bd79fdd75c4e23c723c28
Bekhruz Code <bekhruzbek2022@gmail.com>
2024-08-26 12:41:31 +0900

    fist commit

---
+++ src/myFirstProject.java
@@ -0,0 +1,8 @@
+public class myFirstProject
+{
+    public static void main(String[] args)
+    {
+        System.out.println("May the force be with You!");
+        System.out.println("I did it!!!");
+    }
+}
