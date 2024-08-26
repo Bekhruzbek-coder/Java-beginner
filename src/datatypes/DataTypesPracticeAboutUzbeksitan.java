4ba38fdf4eece1de7b143e69630a6e863bb0953f
Bekhruz Code <bekhruzbek2022@gmail.com>
2024-08-26 11:25:39 +0900

    Data Types Practice about Uzbekistan

---
+++ src/datatypes/DataTypesPracticeAboutUzbeksitan.java
@@ -0,0 +1,40 @@
+package datatypes;
+
+public class DataTypesPracticeAboutUzbeksitan
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Republic of Uzbekistan");
+
+        String capital = "Tashkent";
+        String officialLanguages = "Uzbek";
+        String recognisedRegionalLanguages = "Karakalpak";
+        String ethnicGroups = "Uzbeks, Tajiks, Kazakhs, Karakalpaks, Russians, Others.";
+        String president = "Shavkat Mirziyoyev";
+        String currency = "Uzbek sum (UZS)";
+        String drivingSide = "Right";
+        String internetTLD = ".uz";
+
+        short callingCode = 998;
+
+        int Area = 447400;
+        int population = 37139008;
+
+        float water = 4.9F;
+
+        System.out.println("Capital: " + capital);
+        System.out.println("Official Language: " + officialLanguages);
+        System.out.println("Recognised Regional Language: " + recognisedRegionalLanguages);
+        System.out.println("Ethnic Groups: " + ethnicGroups);
+        System.out.println("President: " + president);
+        System.out.println("Area: " + Area + "km\u00B2");
+        System.out.println("Population: " + population);
+        System.out.println("Water: " + water + "%");
+        System.out.println("Currency: " + currency);
+        System.out.println("Driving Side: " + drivingSide);
+        System.out.println("Calling Code: +" + callingCode);
+        System.out.println("Internet LTD: " + internetTLD);
+
+        System.out.println("~~~~The above information is taken from Wikipedia!!!~~~~");
+    }
+}
