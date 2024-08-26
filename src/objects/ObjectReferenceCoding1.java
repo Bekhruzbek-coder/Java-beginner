049fe314ad4f8cc9bbb990caeca3d8e0c62dc5d8
Bekhruz Code <bekhruzbek2022@gmail.com>
2024-08-26 12:36:14 +0900

    Object Reference Coding 1

---
+++ src/objects/ObjectReferenceCoding1.java
@@ -0,0 +1,32 @@
+package objects;
+
+public class ObjectReferenceCoding1 {
+
+    public static void main(String[] args) {
+        Byte numberOfSeats = 5;
+        Short horsePower = 392;
+        Integer price = 14999;
+        Long registrationNumber = 22424233324234234L;
+
+        Float fuelConsumptionCombined = 15.5F;
+        Double fuelConsumptionPrecise = 15.2323223432243423234;
+
+        Boolean isDamaged = true;
+        Character energyEfficiencyCategory = 'G';
+
+        String carModel = "DodgeChallenger SRT 392";
+        String carModelNew = new String("DodgeChallenger SRT 392");
+
+        System.out.println("Number of seats: " + numberOfSeats);
+        System.out.println("Horse power: " + horsePower);
+        System.out.println("Price: $" + price);
+        System.out.println("Registration number: " + registrationNumber);
+        System.out.println("Combined fuel consumption: " + fuelConsumptionCombined);
+        System.out.println("Precise fuel Consumption: " + fuelConsumptionPrecise);
+        System.out.println("This car is damaged: " + isDamaged);
+        System.out.println("Energy efficiency category: " + energyEfficiencyCategory);
+
+        System.out.println("The car model: " + carModel);
+        System.out.println("The car model with new keyword: " + carModelNew);
+    }
+}
