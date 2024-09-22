6016eb82d1eaa645c48bd232e09dd539e20a1360
Bekhruz Code <bekhruzbek2022@gmail.com>
2024-09-22 11:34:14 +0900

    Operators Code 1

--- src/operators/OperatorsCoding1.java
+++ src/operators/OperatorsCoding1.java
@@ -1,31 +1,29 @@
 package operators;
 
-public class OperatorsCoding1
-{
-    public static void main(String[] args)
-    {
+public class OperatorsCoding1 {
+    public static void main(String[] args){
+
         String carModel = "Dodge Challenger SRT 392";
         int price = 14999;
-        int moneyIntTheBank = 100000;
+        int moneyInTheBank = 100000;
         boolean isDamaged = true;
 
         System.out.println("Price of a " + carModel + ": $" + price);
 
         int increasedPrice = price + 1000;
-        System.out.println("Increased price of a " + carModel + ": $" + increasedPrice);
+        System.out.println("The increased price of a " + carModel + ": $" + increasedPrice);
 
         int decreasedPrice = price - 1000;
         System.out.println("The decreased price of a " + carModel + ": $" + decreasedPrice);
 
-        int twoCarsPrice = price *2;
+        int twoCarsPrice = price * 2;
         System.out.println("Two " + carModel + ": $" + twoCarsPrice);
 
-        int dodgesYouCanBuy = moneyIntTheBank / price;
+        int dodgesYouCanBuy = moneyInTheBank / price;
         System.out.println("From the money we have in the back we can buy " + dodgesYouCanBuy + " " + carModel);
 
-        int moneyRemaining = moneyIntTheBank % price;
-        System.out.println("Money we would remain after buying " + dodgesYouCanBuy + " " + carModel + ": $" + moneyRemaining);
-
+        int moneyRemaining = moneyInTheBank % price;
+        System.out.println("Money we would remain after buying " + dodgesYouCanBuy + " " + carModel + " " + moneyRemaining);
 
     }
 }
