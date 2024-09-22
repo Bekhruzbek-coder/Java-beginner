a53d25903a5119f353a053d01e574e9c0c8959ec
Bekhruz Code <bekhruzbek2022@gmail.com>
2024-09-22 13:05:25 +0900

    Loops Coding 1

--- src/loops/LoopsCoding1.java
+++ src/loops/LoopsCoding1.java
@@ -2,23 +2,23 @@ package loops;
 
 public class LoopsCoding1
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] ars){
+
         int i = 1;
-        while(i <= 5)
-        {
-            System.out.println( i + "BEING RIGHT SUCKS.");
+        while (i <= 5){
+            System.out.println(i + ". BEING RIGHT SUCKS.");
             i++;
         }
         System.out.println("Value of i: " + i);
         System.out.println();
 
         int j = 1;
-        do {
-            System.out.println( j + "BEING RIGHT SUCKS.");
+        do{
+            System.out.println(j + ". BEING RIGHT SUCKS.");
             j++;
-        }
-        while (j <= 5);
+
+        }while(j<=5);
         System.out.println("Value of j: " + j);
+        System.out.println();
     }
 }
