8d0dc08479edc68c4895d1206a1ba0cc87387d71
Bekhruz Code <bekhruzbek2022@gmail.com>
2024-09-22 13:20:07 +0900

    Loops Coding 2

--- src/loops/LoopsCoding2.java
+++ src/loops/LoopsCoding2.java
@@ -1,5 +1,7 @@
 package loops;
 
+import java.sql.SQLOutput;
+
 public class LoopsCoding2
 {
     public static int n = 0;
@@ -12,6 +14,7 @@ public class LoopsCoding2
             i++;
         }
         System.out.println("Value of i: " + i);
+        System.out.println("===========================");
         System.out.println();
 
 
@@ -20,28 +23,25 @@ public class LoopsCoding2
             System.out.println(j + ". BEING RIGHT SUCKS.");
             j++;
         }
-        while(j <= 5);
+        while(j <= 0);
         System.out.println("Value of j: " + j);
+        System.out.println("===========================");
         System.out.println();
 
-
-        for (int k = 1; k <= 5; k++)
-        {
+        for (int k = 1; k <= 5; k++){
             if (k == 3){
                 break;
             }
             System.out.println(k + ". BEING RIGHT SUCKS.");
-            System.out.println();
         }
-        for (int k = 1; k <= 5; k++)
-        {
-            for (int l = 1; l <= 2 ; l++)
-            {
+        System.out.println();
+
+        for (int k = 1; k <= 5; k++) {
+            for (int l = 1; l <= 2; l++){
                 System.out.println("k=" + k + " l=" + l + " BEING RIGHT SUCKS.");
                 n++;
-
             }
         }
-        System.out.println("Printing times: " + n );
+        System.out.println("Printing times: " + n);
     }
 }
